ef95d4469312715341ecab19687ed2896afd1f93
parkkyungin <ruddls1029@gmail.com>
2025-06-20 14:50:34 +0900

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
 # EKS 클러스터 IAM 역할
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
@@ -144,8 +153,4 @@ resource "helm_release" "alb_controller" {
       value = var.service_account_name
     }
   ]
-}
-
-    value = var.vpc_id
-  }
 }
